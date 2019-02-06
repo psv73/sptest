93613ef1c5058a2ecb2e56a67885175a6e7ff945
Sergii Ponomarenko <sergey@psv73.net>
2019-02-06 22:08:00 +0100

    STart MVC

--- springWEB/src/main/java/net/psv73/springweb/MyWebServlet.java
+++ springWEB/src/main/java/net/psv73/springweb/MyWebServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class MyWebServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+//        super.doGet(req, resp);
         resp.getWriter().write("Hello World!");
     }
 }
